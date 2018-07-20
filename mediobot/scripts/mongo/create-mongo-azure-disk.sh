aa36a95fd713bb8c2f919fa3f7b5a9a7d5bf045a
Pierre PIRONIN <pironin.pierre@gmail.com>
2018-07-20 11:11:32 +0200

    Update Mongo persistent disk size.

--- mediobot/scripts/mongo/create-mongo-azure-disk.sh
+++ mediobot/scripts/mongo/create-mongo-azure-disk.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-az disk create --name mongopersistentdisk --resource-group MC_monobot-rg_monobot-aks_eastus --size-gb 20 --query id --output tsv --sku Standard_LRS
+az disk create --name mongopersistentdisk --resource-group MC_monobot-rg_monobot-aks_eastus --size-gb 10 --query id --output tsv --sku Standard_LRS
