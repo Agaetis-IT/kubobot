4af0e4c9d7ff2ed3aaf77a1dd21ab3028de33571
Pierre PIRONIN <pironin.pierre@gmail.com>
2018-08-29 17:11:33 +0200

    Update Istio version to 1.0.1

--- cluster/scripts/istio/install_istio.sh
+++ cluster/scripts/istio/install_istio.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-curl -L https://git.io/getLatestIstio | sh -
-cd istio-0.8.0
-helm template install/kubernetes/helm/istio --name istio --namespace istio-system > istio.yaml
+helm template ~/Tools/istio/istio-1.0.1/install/kubernetes/helm/istio --name istio \
+  --namespace istio-system \
+  --set rbac.create=false \
+  --set serviceAccount.create=false > istio.yaml
 kubectl create namespace istio-system
-kubectl create -f istio.yaml
+kubectl apply -f istio.yaml
