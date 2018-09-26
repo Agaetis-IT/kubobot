1dc513434b9e09ad0cd49c4e13f4b923c1e9dc5e
Pierre PIRONIN <pierre@pierrepironin.fr>
2018-09-26 17:07:07 +0200

    Move from Kubernetes Ingress to Istio Gateway/VirtualService

--- cluster/scripts/ingress/install-cert-manager.sh
+++ cluster/scripts/ingress/install-cert-manager.sh
@@ -2,4 +2,7 @@
 helm install stable/cert-manager --namespace kube-system \
   --name cert-manager \
   --set rbac.create=false \
-  --set serviceAccount.create=false
+  --set serviceAccount.create=false \
+  --set podDnsConfig.nameservers[0]=40.90.4.7 \
+  --set podDnsConfig.nameservers[1]=64.4.48.7 \
+  --set podDnsConfig.nameservers[2]=13.107.24.7
